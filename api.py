a62f16af7e25c79c9b35b44648719d92890e2aea
Pete Emerson <pete@automatic.com>
2015-11-13 19:11:01 +0000

    Reading from the db

--- api.py
+++ api.py
@@ -1,5 +1,34 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+# configuration
+DATABASE = '/home/pete/db/fender.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'fender'
+PASSWORD = 'fenderpw'
+
 app = Flask(__name__)
+
+def connect_db():
+    return sqlite3.connect(DATABASE)
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.route('/db')
+def db_get():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    e = []
+    for entry in entries:
+        e.append({'title':entry[0],'text':entry[1]})
+    print "Entries: %s" % e
+    return render_template('show_entries.html', entries=e)
+
 
 @app.route('/')
 def hello_world():
